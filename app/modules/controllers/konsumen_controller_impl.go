0f69e113753f95a4b8c4c69dbc6989295e95101c
Isa Afisyah <isaafisyah@gmail.com>
2025-06-08 19:16:36 +0700

    add log module konsumen

--- app/modules/controllers/konsumen_controller_impl.go
+++ app/modules/controllers/konsumen_controller_impl.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/isaafisyah/studi-kasus-multifinance/app/log"
 	"github.com/isaafisyah/studi-kasus-multifinance/app/modules/dto"
 	"github.com/isaafisyah/studi-kasus-multifinance/app/modules/repositories"
 	"github.com/isaafisyah/studi-kasus-multifinance/app/modules/services"
@@ -19,8 +20,10 @@ func NewKonsumenController(konsumenService services.KonsumenService, repo reposi
 }
 
 func (c *KonsumenControllerImpl) FindAll(ctx *gin.Context)  {
+	log.GetLogger("KonsumenController").Info("All Konsumen Start")
 	konsumens, err := c.konsumenService.FindAll(ctx)
 	if err != nil {
+		log.GetLogger("KonsumenController").Error(err.Error())
 		ctx.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
@@ -28,39 +31,48 @@ func (c *KonsumenControllerImpl) FindAll(ctx *gin.Context)  {
 }
 
 func (c *KonsumenControllerImpl) FindById(ctx *gin.Context)  {
+	log.GetLogger("KonsumenController").Info("Detail Konsumen Start")
 	idStr := ctx.Param("id")
 	konsumen, err := c.konsumenService.FindById(idStr)
 	if err != nil {
+		log.GetLogger("KonsumenController").Error(err.Error())
 		ctx.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
+	log.GetLogger("KonsumenController").Info("Detail Konsumen End")
 	ctx.JSON(200, gin.H{"data": konsumen})
 }
 
 func (c *KonsumenControllerImpl) Create(ctx *gin.Context)  {
+	log.GetLogger("KonsumenController").Info("Create Konsumen Start")
 	var req dto.CreateKonsumenRequest
 	if err := ctx.ShouldBind(&req); err != nil {
+		log.GetLogger("KonsumenController").Error(err.Error())
 		ctx.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
 
 	konsumen, err := c.konsumenService.Create(req, ctx)
 	if err != nil {
+		log.GetLogger("KonsumenController").Error(err.Error())
 		ctx.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
 
 	if err := c.repo.Save(*konsumen); err != nil {
+		log.GetLogger("KonsumenController").Error(err.Error())
 		ctx.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
-
+	log.GetLogger("KonsumenController").Info("Create Konsumen End")
 	ctx.JSON(http.StatusCreated, gin.H{"message" : "data berhasil disimpan"})
 }
 
 func (c *KonsumenControllerImpl) Update(ctx *gin.Context)  {
+	log.GetLogger("KonsumenController").Info("Update Konsumen Start")
 	var req dto.UpdateKonsumenRequest
 	if err := ctx.ShouldBind(&req); err != nil {
+		log.GetLogger("KonsumenController").Error(err.Error())
 		ctx.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
@@ -68,30 +80,37 @@ func (c *KonsumenControllerImpl) Update(ctx *gin.Context)  {
 	id := ctx.Param("id")
 	konsumen, err := c.konsumenService.Update(id , req)
 	if err != nil {
+		log.GetLogger("KonsumenController").Error(err.Error())
 		ctx.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
 
 	data, err := c.repo.Update(*konsumen)
 	if err != nil {
+		log.GetLogger("KonsumenController").Error(err.Error())
 		ctx.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
+	log.GetLogger("KonsumenController").Info("Update Konsumen End")
 	ctx.JSON(200, gin.H{"data": data})
 }
 
 func (c *KonsumenControllerImpl) Delete(ctx *gin.Context)  {
+	log.GetLogger("KonsumenController").Info("Delete Konsumen Start")
 	idParam := ctx.Param("id")
 	konsumen, err := c.konsumenService.FindById(idParam)
 	if err != nil {
+		log.GetLogger("KonsumenController").Error(err.Error())
 		ctx.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
 
 	if err := c.repo.Delete(konsumen); err != nil {
+		log.GetLogger("KonsumenController").Error(err.Error())
 		ctx.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
+	log.GetLogger("KonsumenController").Info("Delete Konsumen End")
 	ctx.JSON(200, gin.H{"message": "data berhasil dihapus"})
 }
 
